d5d719043220af0dff1c3a629ff7bf766b2753d4
murano500k <murano500k@gmail.com>
2017-04-05 11:24:13 +0300

    socket rx in progress

--- app/src/main/java/com/stc/smartbulb/rx2/Rx2Model.java
+++ app/src/main/java/com/stc/smartbulb/rx2/Rx2Model.java
@@ -4,15 +4,10 @@ import android.util.Log;
 
 import com.stc.smartbulb.model.Device;
 
-import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.StringJoiner;
-import java.util.concurrent.Callable;
 
 import io.reactivex.Completable;
 import io.reactivex.CompletableEmitter;
@@ -20,14 +15,18 @@ import io.reactivex.CompletableOnSubscribe;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.Single;
 import io.reactivex.SingleEmitter;
 import io.reactivex.SingleOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+
+import static io.reactivex.Single.create;
 
 /**
  * Created by artem on 4/4/17.
@@ -60,7 +59,7 @@ public class Rx2Model implements Rx2Contract.Presenter{
     @Override
     public void start() {
         Log.i(TAG, "start: ");
-        mDisposable.add(Single.create(new SingleOnSubscribe<Device>() {
+        mDisposable.add(create(new SingleOnSubscribe<Device>() {
             @Override
             public void subscribe(SingleEmitter<Device> e) throws Exception {
                 e.setCancellable(() -> {
@@ -99,32 +98,13 @@ public class Rx2Model implements Rx2Contract.Presenter{
 
     @Override
     public void click(){
-
-        mDisposable.add(Observable.fromCallable(new Callable<Device>() {
-            @Override
-            public Device call() throws Exception {
-                mDevice.setTurnedOn(!mDevice.isTurnedOn());
-                mSocket=mDeviceManager.connectToDevice(mDevice);
-                mSocket.setKeepAlive(true);
-                readLogs(new BufferedReader(new InputStreamReader(mSocket.getInputStream())));
-                mDeviceManager.writeCmd(mDevice.isTurnedOn(), mSocket);
-                return mDevice;
-            }
-        })
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(new Consumer<Device>() {
-            @Override
-            public void accept(Device device ) throws Exception {
-                view.deviceReady(device);
-            }
-        }, throwable -> {
-                    throwable.printStackTrace();
-                    view.deviceLost(throwable.getMessage());
-                }));
+        if(mDevice==null) view.deviceLost("null");
+        else {
+            sendCmd(!mDevice.isTurnedOn());
+        }
     }
-    public Observable<String> sendCmd(boolean cmd){
-        Single.create(new SingleOnSubscribe<Socket>() {
+    public void sendCmd(boolean cmd){
+        Single <Socket>single=Single.create(new SingleOnSubscribe<Socket>() {
             @Override
             public void subscribe(SingleEmitter<Socket> e) throws Exception {
                 if(mSocket==null || mSocket.isClosed()) mSocket = mDeviceManager.connectToDevice(mDevice);
@@ -137,7 +117,35 @@ public class Rx2Model implements Rx2Contract.Presenter{
                 mDeviceManager.writeCmd(cmd , socket);
                 return socket;
             }
-        }).flatMapPublisher(new Function<Socket, Publisher<?>>() {
+        });
+        single.flatMapObservable(new Function<Socket, ObservableSource<String>>() {
+            @Override
+            public ObservableSource<String> apply(Socket socket) throws Exception {
+                return Observable.create(new ObservableOnSubscribe<String>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<String> e) throws Exception {
+                        BufferedReader mBos= new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
+                        while (mSocket!=null && mSocket.isConnected()){
+                            if(mBos.ready())e.onNext(mBos.readLine());
+                        }
+                        e.onComplete();
+                    }
+                });
+            }
+        }).subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        Log.d(TAG, "new line: " + s);
+                    }
+                }, Throwable::printStackTrace, new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        view.deviceReady(mDevice);
+                    }
+                });
+
+        /*.flatMapPublisher(new Function<Socket, Publisher<?>>() {
             @Override
             public Publisher<?> apply(Socket socket) throws Exception {
                 return new Publisher<String>() {
@@ -152,18 +160,8 @@ public class Rx2Model implements Rx2Contract.Presenter{
                     }
                 };
             }
-        });
-        Observable.create(new ObservableOnSubscribe<String>() {
-            @Override
-            public void subscribe(ObservableEmitter<String> e) throws Exception {
+        });*/
 
-                if(mSocket==null || mSocket.isClosed()) mSocket = mDeviceManager.connectToDevice(mDevice);
-                mDeviceManager.writeCmd(cmd,mSocket);
-                BufferedReader mBos= new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
-                while (mSocket!=null && mSocket.isConnected()){
-                    if(mBos.ready())e.onNext(mBos.readLine());
-                }
-            }
-        });
+
     }
 }
